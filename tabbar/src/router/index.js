29707d5c514af8ddbdcf7f584736eb077aa72381
Oliverxuan <2373734127@qq.com>
2021-06-15 20:05:32 +0800

    Version：1.0.0

---
+++ tabbar/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Router from "vue-router";
+const Home = () => import("../views/home/Home.vue");
+const Category = () => import("../views/category/Category.vue");
+const Profile = () => import("../views/profile/Profile.vue");
+const Cart = () => import("../views/cart/Cart.vue");
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "",
+      redirect: "/home"
+    },
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/category",
+      component: Category
+    },
+    {
+      path: "/profile",
+      component: Profile
+    },
+    {
+      path: "/cart",
+      component: Cart
+    }
+  ],
+  mode: "history"
+});
